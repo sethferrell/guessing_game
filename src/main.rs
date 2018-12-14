15bc6ee38afec26ef7c04c0aaaba4f208f1d33f8
sethferrell <45112803+sethferrell@users.noreply.github.com>
2018-12-14 15:43:38 -0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -6,32 +6,49 @@ use rand::Rng;
 
 fn main() {
 	println!("Guess the number!");
-
-	let secret_number = rand::thread_rng().gen_range(1, 101);
-
-	//println!("The secret number is: {}", secret_number);
 	loop {
-	println!("Please input your guess.");
-    
-	let mut guess = String::new();
+		let secret_number = rand::thread_rng().gen_range(1, 101);
 
-	io::stdin().read_line(&mut guess)
-		.expect("Failed to read line");
+		loop {
+		println!("Please input your guess.");
 
-	let guess: u32 = match guess.trim().parse() {
-		Ok(num) => num,
-		Err(_) => continue,
-	};
+		let mut guess = String::new();
 
-	println!("You guessed: {}", guess);
+		io::stdin().read_line(&mut guess)
+			.expect("Failed to read line");
 
-	match guess.cmp(&secret_number) {
-	Ordering::Less => println!("Too small!"),
-	Ordering::Greater => println!("Too big!"),
-	Ordering::Equal => {
-		println!("You win!");
-		break;
+		let guess: u32 = match guess.trim().parse() {
+			Ok(num) => num,
+			Err(_) => continue,
+		};
+
+		println!("You guessed: {}", guess);
+
+		match guess.cmp(&secret_number) {
+		Ordering::Less => println!("Too small!"),
+		Ordering::Greater => println!("Too big!"),
+		Ordering::Equal => {
+			println!("You win!");
+			break;
+				}
+			}
 		}
-		}
+	println!("Do you want to play again? (q to quit)");
+	
+	let mut quit = String::new();
+		
+	io::stdin().read_line(&mut quit)
+		.ok()
+		.expect("Can't read line");
+	
+	match quit.trim() {
+		"q" => {
+			println!("Goodbye!");
+			println!(" ");
+			break;
+		},
+		_ => println!("Lets continue")
+	}
+	println!(" ");
 	}
 }
